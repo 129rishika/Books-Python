2e20d22fb50435ec937885c5ad819f5f57e17da4
129rishika <125148873+129rishika@users.noreply.github.com>
2023-02-12 00:29:19 +0530

    Add files via upload

---
+++ my_second_ program.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Types
+
+# In[1]:
+
+
+x=5
+type(x)
+
+
+# In[3]:
+
+
+x=1.7
+type(x)
+
+
+# In[5]:
+
+
+name="rishika"
+type(name)
+
+
+# In[6]:
+
+
+z=True
+type(z)
+
+
+# In[ ]:
+
+
+### integer(Number without decimal)
+### float(Number with decimal)
+### str string(text data type)
+### bool boolean(True/False)
+
+
+# In[ ]:
+
+
+
+
